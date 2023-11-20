4213c3689b90cd10188dad926aaf010f250388db
Felipe-Baamonde <felipe.baamonde@sptech.school>
2023-11-20 14:07:46 -0300

    Mostrando nome e plano do usuario no Connect

--- Algoritmo/Site-Flutua/site/public/js/sessao.js
+++ Algoritmo/Site-Flutua/site/public/js/sessao.js
@@ -2,11 +2,14 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var planoId = sessionStorage.ID_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
-
+    var plano_usuario = document.getElementById("plano_usuario");
+    
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
+        plano_usuario.innerHTML = planoId
     } else {
         window.location = "../Login.html";
     }
